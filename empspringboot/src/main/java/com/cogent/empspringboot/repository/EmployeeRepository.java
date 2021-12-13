f9e2e46fd8c2d0d34ce7488a115361e59956d098
LeviCostello <46667507+LeviCostello@users.noreply.github.com>
2021-12-13 11:59:44 -0500

    Delete empspringboot directory

--- empspringboot/src/main/java/com/cogent/empspringboot/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cogent.empspringboot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.cogent.empspringboot.dto.Employee;
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
-}
